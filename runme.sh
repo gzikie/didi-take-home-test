d77c4294ec8c9c728b6db4b72b1f02e7a23a596c
Zikie Gao <ziqi.gao@saymosaic.com>
2019-09-21 23:16:20 -0700

    modifies runme.sh file and added some logs

--- runme.sh
+++ runme.sh
@@ -1,38 +1,5 @@
-#!/bin/sh
+printf "start to find words. \n"
 
-# THE_CLASSPATH=
-# PROGRAM_NAME=findWords.java
-# CLASS_NAME=findWords
+java findWords
 
-# cd src
-# for i in `ls ../lib/*.jar`
-#     do
-#     THE_CLASSPATH=${THE_CLASSPATH}:${i}
-# done
-
-# javac -classpath ".:${THE_CLASSPATH}" $PROGRAM_NAME
-
-# if [ $? -eq 0 ]
-# then
-#     echo "compile worked!"
-# fi
-
-# java -classpath ".:${THE_CLASSPATH}" $CLASS_NAME
-# then
-# 	echo "finished finding words"
-# fi
-
-text=""
-text+="q: quit.\n"
-
-
-#printf "$text"
-selected=0
-while [ ${selected} == 0 ]
-do
-	javac ./findWords.java
-
-	java ./findWords
-
-
-done
+printf "finished finding all words that are composed of given characters! \n"
