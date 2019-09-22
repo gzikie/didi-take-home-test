eecf99e9554cd7beacbc6ff541005e067dba8e7a
Zikie Gao <ziqi.gao@saymosaic.com>
2019-09-21 18:36:51 -0700

    need to work on runme.sh

--- runme.sh
+++ runme.sh
@@ -1,23 +1,38 @@
 #!/bin/sh
 
-THE_CLASSPATH=
-PROGRAM_NAME=findWords.java
-CLASS_NAME=findWords
+# THE_CLASSPATH=
+# PROGRAM_NAME=findWords.java
+# CLASS_NAME=findWords
 
-cd src
-for i in `ls ../lib/*.jar`
-    do
-    THE_CLASSPATH=${THE_CLASSPATH}:${i}
+# cd src
+# for i in `ls ../lib/*.jar`
+#     do
+#     THE_CLASSPATH=${THE_CLASSPATH}:${i}
+# done
+
+# javac -classpath ".:${THE_CLASSPATH}" $PROGRAM_NAME
+
+# if [ $? -eq 0 ]
+# then
+#     echo "compile worked!"
+# fi
+
+# java -classpath ".:${THE_CLASSPATH}" $CLASS_NAME
+# then
+# 	echo "finished finding words"
+# fi
+
+text=""
+text+="q: quit.\n"
+
+
+#printf "$text"
+selected=0
+while [ ${selected} == 0 ]
+do
+	javac ./findWords.java
+
+	java ./findWords
+
+
 done
-
-javac -classpath ".:${THE_CLASSPATH}" $PROGRAM_NAME
-
-if [ $? -eq 0 ]
-then
-    echo "compile worked!"
-fi
-
-java -classpath ".:${THE_CLASSPATH}" $CLASS_NAME
-then
-	echo "finished finding words"
-fi
